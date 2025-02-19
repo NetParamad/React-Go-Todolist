0f0527bb5cbdb4ebb0bc73211341c3b096d9dbbf
NetParamad <netparamad@gmail.com>
2025-02-19 21:32:20 +0700

    Updata

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	fmt.Println("Hello World")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{
+			"message": "Hello World",
+		})
+	})
+
+	log.Fatal(app.Listen(":3001"))
+
+
+}
